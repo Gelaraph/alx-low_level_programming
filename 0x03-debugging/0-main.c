b447bf2836600eea58d5a031ad25b2cd7135ce81
Angela Raphael <angelaraph12@gmail.com>
2022-04-13 01:03:46 -0700

    Completed

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* main - tests that the function positive_or_negative() gives the correct output when given a case of 0.
+* main - tests function that prints if integer is positive or negative
 * Return: 0
 */
 
@@ -11,7 +11,6 @@ int main(void)
 
 	i = 0;
 	positive_or_negative(i);
-
 
 	return (0);
 }
