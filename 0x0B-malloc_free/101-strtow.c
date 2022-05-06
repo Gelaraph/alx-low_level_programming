363cc4700dc4a243c4ba66494948f7d15cb3b759
Angela Raphael <angelaraph12@gmail.com>
2022-05-06 20:17:54 +0100

    Update 101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -13,7 +13,7 @@ char **strtow(char *str)
 	int i, j, k = 0, l, m, count = 0, len;
 	char **words;
 
-	if (str == NULL || str == '\0')
+	if (str == NULL || str[0] == '\0')
 		return (NULL);
 
 	for (i = 0; str[i] != '\0'; i++)
